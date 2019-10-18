57c41bb44f252e6d8052e3ab0eea596c1fe27963
Lles <alessiocellucci@gmail.com>
2019-10-18 20:06:40 +0200

    Cambiato lo scheletro del CPP reference

--- public/js/index.js
+++ public/js/index.js
@@ -28,6 +28,42 @@ fadeOut = function (elem) {
     }
 }
 
+openContainer = function (elem) {
+    let x = elem.getElementsByTagName("p");
+    let y = elem.getElementsByClassName("partDescription");
+
+    elem.classList.remove("containerImpand");
+    elem.classList.add("containerExpand");
+    elem.classList.remove("impanded");
+    elem.classList.add("expanded");
+
+    x[0].classList.remove("slideDown");
+    x[0].classList.add("slideUp");
+    x[0].classList.add("disappear");
+
+    y[0].classList.remove("slideUp");
+    y[0].classList.add("slideDown");
+    y[0].classList.add("appear");
+}
+
+closeContainer = function (elem) {
+    let x = elem.getElementsByTagName("p");
+    let y = elem.getElementsByClassName("partDescription");
+
+    elem.classList.remove("containerExpand");
+    elem.classList.add("containerImpand");
+    elem.classList.remove("expanded");
+    elem.classList.add("impanded");
+
+    x[0].classList.remove("disappear");
+    x[0].classList.remove("slideUp");
+    x[0].classList.add("slideDown");
+
+    y[0].classList.remove("slideDown");
+    y[0].classList.remove("appear");
+    y[0].classList.add("slideUp");
+}
+
 toPage = function (pathToPage) {
     location.href=`${pathToPage}`;
 }
